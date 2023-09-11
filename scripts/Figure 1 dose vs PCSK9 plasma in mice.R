875ac5dff29048df7f6b12ccd9376571956f7053
Marybeth Soutar <mbsoutar@gmail.com>
2023-09-11 00:09:12 -0400

    manual entry for 0.25 ug/g

--- scripts/Figure 1 dose vs PCSK9 plasma in mice.R
+++ scripts/Figure 1 dose vs PCSK9 plasma in mice.R
@@ -26,5 +26,39 @@ library(rafalib)
 library(sourcetools)
 library(htmltools)
 library(plotrix)
-#we added all packages
+# we added all packages
+# catching up on variables and functions
+first_num <- 42
+first_num
+my_obj2<-"R is cool."
+my_obj2
+my_obj2<-1024
+my_obj<-48
+my_obj3<-my_obj2+my_obj
+rm(first_num)
+ls()
+apropos()
+apropos(plot)
+apropos("plot")
+help.search("plot")
+??plot
+list.files()
+list.dirs()
+# Loading [liver] by dose - 0.25 ug/g 
+# Rounding mean
+liver_exp_w_0.25<-c(1.40,1.56,1.47,1.27)
+dose_0.25_mean<-mean(liver_exp_w_0.25) 
+dose_0.25_mean
+dose_0.25_mean<-round(mean(liver_exp_w_0.25),2) 
+dose_0.25_mean
+# Rounding std. dev.
+dose_0.25_s.d.<-round(sd(liver_exp_w_0.25),2)
+dose_0.25_s.d.
+# Rounding standard error of means 
+dose_0.25_std.error<-round(std.error(liver_exp_w_0.25),2)
+dose_0.25_std.error
 
+#Vector from table
+liver_exp_w_0.25<-c(1.40,1.56,1.47,1.27)
+dose_0.25_mean<-mean(liver_exp_w_0.25) 
+dose_0.25_mean
